b739eb20c4733e1bc7ac53150f44ac9c116cfd5c
halaltomillion-png <halaltomillion@gmail.com>
2025-12-24 14:11:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,39 @@ import express from "express";
 
 const app = express();
 
+// ENV VARS
+const CLIENT_ID = process.env.WHOP_CLIENT_ID;
+const REDIRECT_URI = process.env.WHOP_REDIRECT_URI;
+
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Backend läuft");
+});
+
+// LOGIN START
+app.get("/login", (req, res) => {
+  const whopUrl =
+    "https://whop.com/oauth/authorize" +
+    `?client_id=${CLIENT_ID}` +
+    `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+    `&response_type=code`;
+
+  res.redirect(whopUrl);
+});
+
+// CALLBACK VON WHOP
+app.get("/callback", (req, res) => {
+  const code = req.query.code;
+
+  if (!code) {
+    return res.send("Kein Code von Whop erhalten");
+  }
+
+  res.send(`
+    <h1>Login erfolgreich</h1>
+    <p>Whop hat geantwortet.</p>
+    <p>Code: ${code}</p>
+  `);
 });
 
 export default app;
